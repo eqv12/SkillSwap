3363d61b36ffd7882f256b4d0e9c8b84ae54b4cd
sharun <sharun.offl@gmail.com>
2024-11-23 13:54:58 +0530

    in the testing.js file added the sample testing code to retreive all the requests that a tutor should see. Need more work to change senderID to name and subject name

--- testing.js
+++ testing.js
@@ -1,8 +1,28 @@
 import { User } from "./models/User.js";
 import { Skill } from "./models/Skills.js";
 import connectDB from "./db.js";
+// import { Skill } from "./models/Skills.js";
+import { Request } from "./models/Request.js";
+import Counter from "./models/Counter.js";
+// import connectDB from "./db.js";
 
-connectDB();
+await connectDB();
 
-const userWithSkills = await User.findOne({ name: 'Ramya' }).populate('skills');
-console.log(userWithSkills);
+const test = await User.findOne({ rollno: "24MX121" });
+console.log(test);
+const user = await User.findOne({ rollno: "24MX121" }, { _id: 0, skills: 1 });
+const userSkills = user?.skills;
+console.log(userSkills);
+const final = await Request.find(
+  {
+    subjectId: { $in: userSkills },
+    status: "Pending",
+  },
+  {
+    senderId: 1,
+    description: 1,
+    subjectId: 1,
+    _id: 0,
+  }
+);
+console.log(final);
