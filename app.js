fb53a7ffc28a590319e5428f7aa1e610862a4873
Ramya Raja <ramyaraja1206@gmail.com>
2024-12-11 13:06:40 +0530

    Changed callback url

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/callback",
+      callbackURL: "https://skillswap-hh05.onrender.com/auth/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
